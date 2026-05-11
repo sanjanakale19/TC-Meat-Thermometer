6665ff628c1c7c6e5abfb399146b32932e3af393
reillyamira <reilly@amira.care>
2026-05-11 14:23:31 -0700

    Add pin definitions and file outline

---
+++ src/pins.h
@@ -0,0 +1,34 @@
+/*!
+ * @file Pins.h
+ *
+ * Pin assignments
+ *
+ * Written by Reilly
+ *
+ */
+
+#ifndef PINS_H
+#define PINS_H
+
+// I2C
+#define PIN_I2C_SCL 21
+#define PIN_I2C_SDA 48
+
+// Analog inputs (NEED TO ADD POT_VREF ONCE FINAL SOLN)
+#define PIN_VMEASURE 9
+
+// Digital outputs / inputs
+#define PIN_COMPARATOR_OUT 13
+#define PIN_MUX_A0 40
+#define PIN_MUX_A1 41
+#define PIN_SIGNAL_SEL 42
+#define PIN_ADC_RESET 18
+#define DATA_NEG 19
+#define DATA_POS 20
+
+// Max31855
+#define PIN_CS_MAX31855 4
+#define PIN_VSCLK 16
+#define PIN_VMISO 17
+
+#endif PINS_H
